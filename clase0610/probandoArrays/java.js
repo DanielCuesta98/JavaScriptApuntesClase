c63405ec3d768bebf160cb5832919fc2b2a2ef82
Daniel Cuesta <danicu988@gmail.com>
2023-10-11 09:16:02 +0200

    clase 1110

---
+++ clase0610/probandoArrays/java.js
@@ -0,0 +1,10 @@
+
+//PROBANDO ARRAYS Y SUS METODOS
+let a = ["lunes", "martes", 2,4,6];
+let cadena = a.join(" : ");
+console.log(cadena);
+
+let notas = "5-7.5-6-5";
+
+let notasArray = notas.split("-");
+console.log(notasArray);
